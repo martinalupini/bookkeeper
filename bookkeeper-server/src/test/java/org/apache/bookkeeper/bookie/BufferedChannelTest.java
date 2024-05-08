cebbfe23ec3218e236080614c9ea288421fbf5d9
martinalupini <martinalupini11@gmail.com>
2024-05-08 19:16:41 +0200

    First attempt to add Jacoco

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BufferedChannelTest.java
@@ -0,0 +1,15 @@
+package org.apache.bookkeeper.bookie;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BufferedChannelTest {
+
+    @Test
+    public void dummyTest(){
+        int a=1;
+        int b=2;
+
+        Assert.assertEquals(3, a+b);
+    }
+}
