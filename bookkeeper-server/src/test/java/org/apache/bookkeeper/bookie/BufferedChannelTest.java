516d89af6d606699f50954dc07cc20d26dd42485
martinalupini <martinalupini11@gmail.com>
2024-05-15 16:42:48 +0200

    added Jacoco. Coverage now works (added skipTest in main pom.. need to delete later maybe). Removed unnecessary dependencies and modules in main pom and tests pom

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BufferedChannelTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BufferedChannelTest.java
@@ -1,15 +1,91 @@
 package org.apache.bookkeeper.bookie;
 
-import org.junit.Assert;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BufferedChannelTest {
 
-    @Test
-    public void dummyTest(){
-        int a=1;
-        int b=2;
+    private File tempFile;
+    private FileChannel fileChannel;
+    private BufferedChannel bufferedChannel;
 
-        Assert.assertEquals(3, a+b);
+    @Before
+    public void setUp() throws IOException {
+        tempFile = Files.createTempFile("test", ".tmp").toFile();
+        fileChannel = FileChannel.open(tempFile.toPath());
+        bufferedChannel = new BufferedChannel(ByteBufAllocator.DEFAULT, fileChannel, 1024);
     }
+
+    @After
+    public void tearDown() throws IOException {
+        bufferedChannel.close();
+        fileChannel.close();
+        Files.delete(tempFile.toPath());
+    }
+
+    @Test
+    public void testPosition(){
+        long pos = bufferedChannel.position();
+
+        assertEquals(0, pos);
+
+    }
+
+    /*
+    @Test
+    public void testWriteAndRead() throws IOException {
+        // Write data to the channel
+        byte[] testData = "Hello, world!".getBytes();
+        ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer();
+        byteBuf.writeBytes(testData);
+        bufferedChannel.write(byteBuf);
+
+        // Reset position to read back the data
+        bufferedChannel.forceWrite(true);
+        bufferedChannel.clear();
+        long position = bufferedChannel.getFileChannelPosition();
+
+        // Read data from the channel
+        ByteBuffer readBuffer = ByteBuffer.allocate(testData.length);
+        fileChannel.position(position);
+        fileChannel.read(readBuffer);
+        readBuffer.flip();
+        byte[] readData = new byte[readBuffer.remaining()];
+        readBuffer.get(readData);
+
+        // Verify the read data
+        assertEquals(new String(readData), new String(readData));
+    }
+
+    @Test
+    public void testForceWrite() throws IOException {
+        // Write data to the channel
+        byte[] testData = "Hello, force write!".getBytes();
+        ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer();
+        byteBuf.writeBytes(testData);
+        bufferedChannel.write(byteBuf);
+
+        // Force write
+        long positionBeforeForce = bufferedChannel.forceWrite(true);
+
+        // Verify that force write updated the position
+        assertTrue(0==0);
+    }
+
+     */
+
+
+
 }
